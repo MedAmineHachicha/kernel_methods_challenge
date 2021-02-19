b9ccbce64ac1f33cb902397e821a854901c911d5
KhalilBergaoui <45513583+KhalilBergaoui@users.noreply.github.com>
2021-02-19 15:29:36 +0100

    Update models.py
    added KLR (with gaussian kernel only)

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cvxopt
 from tqdm import tqdm
+import scipy
+from scipy.spatial.distance import pdist, squareform, cdist
+from scipy.special import expit
 
 class LogisticRegression():
 
@@ -108,3 +111,176 @@ class SVMClassifier():
             y_pred[i] = (s > 0) * 1
 
         return y_pred
+    
+    
+ class WKRR():
+    """
+    Weighted Kernel Ridge Regression
+    
+    """
+
+    def __init__(self,kernel='gaussian'):
+        self.weights = None
+        self.kernel = kernel
+        #kernel gram matrix over training data 
+        self.K_train = None
+        #training samples
+        self.X_train = None
+        
+
+
+    def get_kernel_gram_matrix(X,sigma):
+        
+        if self.kernel == 'gaussian':
+            
+            #Faster computation of the gram matrix with gaussian kernel
+            #st= time.time()
+            pairwise_dists = squareform(pdist(X, 'sqeuclidean'))
+            K = np.exp(-pairwise_dists /(2*np.square(sigma)))
+            #print(time.time()-st)
+            return K
+
+    
+    def fit(self,X,y,penalty,sigma,W=None, eps=1e-6,kernel_precomputed = False):
+        """
+        Returns analytical solution of the Weighted Kernel Ridge Regression problem
+        """
+        
+        
+        self.X_train = X
+        if kernel_precomputed:
+            K = self.K_train
+        else:
+            K = self.get_kernel_gram_matrix(X,sigma)
+            self.K_train = K
+        
+        assert K.shape[0] == y.shape[0]
+        n = K.shape[0]
+        
+        if W is None:
+            #unweighted KLR := all weights are equal to 1 and W:=Identity
+            M = K@y  + n*penalty*np.eye(n)
+            M_ = scipy.linalg.inv(M)
+            v = M_@v
+            
+        else:
+            
+            W_sqrt = np.diag(np.sqrt(np.diag(W)))
+            v = W_sqrt@y
+            M = K@W_sqrt
+            M = W_sqrt@M + n*penalty*np.eye(n)
+            M_ = scipy.linalg.inv(M)
+            v = M_@v
+            v = W_sqrt@v
+            
+        print('fitted train data')  
+        self.weights = v
+        
+    
+    
+class KernelLogisticRegression():
+    """
+    Kernel Logistic regression
+    """
+
+    def __init__(self,kernel='gaussian'):
+        self.weights = None
+        self.kernel = kernel
+        self.loss_thresh = 0.001
+        self.X_train = None
+        
+        #initialize weighted kernel ridge regression for self.fit
+        self.wkrr = WKRR()
+        
+
+
+    def get_kernel_gram_matrix(X,sigma):
+        
+        if self.kernel == 'gaussian':
+            
+            #Faster computation of the gram matrix with gaussian kernel
+            #st= time.time()
+            pairwise_dists = squareform(pdist(X, 'sqeuclidean'))
+            K = np.exp(-pairwise_dists /(2*np.square(sigma)))
+            #print(time.time()-st)
+            return K
+
+    def fit(self, X, y,penalty,sigma, max_iter=1000, eps=1e-6,kernel='gaussian'):
+        """
+        Iteratively solve Weighted Kernel Ridge Regression problems
+        """
+        
+        self.X_train = X
+        K = get_kernel_gram_matrix(X,sigma)
+        self.wkrr.K_train = K
+        
+        #For training only, transform labels in [1,-1]
+        y = np.where(y==1,1,0)
+        
+        assert K.shape[0] == y.shape[0]
+        n = K.shape[0]
+        ones = np.ones(y.shape)
+       
+    
+        #randomly initialize the coefficients
+        alpha = np.random.normal(loc=0,scale=1,size = n)
+
+        #t1 = time.time()
+
+        #initialize loss
+        loss = 10
+
+        for i in range(max_iter):
+            #At each iteration solve a Weighted kernel ridge regression
+            v = K@alpha
+            prev_loss = loss
+            loss = -np.sum(np.log(expit(np.multiply(y,v)) + eps))/n
+            print(loss)
+            if np.abs(loss-prev_loss)<self.loss_thresh:
+                print('converged after {} iterations'.format(i+1))
+                break
+            
+            #compute parameters for WKRR
+            u = np.multiply(v,y)
+            sig = expit(u) 
+            sig_ = ones-sig # 1-sig = expit(-u)
+            W = np.diag(np.multiply(sig,(sig_)))
+            #print(W.shape)
+            P = np.diag(-sig_)
+            k = P@y
+            z = v - scipy.linalg.inv(W)@k
+            #t2 = time.time()
+            #print(t2-t1)
+            
+            #solve a weighted Kernel Ridgre Regression with the corresponding parameters
+            alpha = self.wkrr.fit(X,z,penalty,sigma,W=W,kernel_precomputed=True)
+            #t3 = time.time()
+            #print(t3-t2)
+        
+        #save fitted parameters
+        self.weights = alpha
+        
+        
+    def predict_probas(self, X):
+        
+        try:
+            assert self.kernel =='gaussian'
+            #compute pairwise (squared euclidean) distances between new samples and train samples
+            pairwise_dists = cdist(self.X_train,X,'sqeuclidean')
+            #gaussian kernel evaluations
+            K_pred = np.exp(-pairwise_dists /(2*np.square(sigma)))
+
+            pred_probas = expit(K_pred.T@self.weights) 
+            return pred_probas
+        
+        except:
+            print('Please make sure the used kernel is gaussian.')
+            
+        
+    def predict(self, X):
+        probas = self.predict_probas(X=X)
+        return (probas>0.5).astype(int)
+
+    def get_accuracy_score(self, X, y):
+        pred_labels = self.predict(X=X)
+        return (pred_labels==y).mean()
